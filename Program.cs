60162274bd839b942b7de5583fe336873285cfe3
Bruno Leone Alexandre Pacheco Corrêa <leone.analistadesistemas@outlook.com.br>
2016-10-20 11:59:04 -0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,5 +11,26 @@ namespace OrdenacaoInsertionShort
         static void Main(string[] args)
         {
         }
+
+        public static int[] insertionShort(int[] vetor)
+        {
+            int i, j, atual;
+
+            for (i = 1; i < vetor.Length; i++)
+            {
+                atual = vetor[i];
+
+                j = i;
+
+                while ((j > 0) && (vetor[j - 1] > atual))
+                {
+                    vetor[j] = vetor[j - 1];
+
+                    j = j - 1;
+                }
+                vetor[j] = atual;
+            }
+            return vetor;
+        }
     }
 }
